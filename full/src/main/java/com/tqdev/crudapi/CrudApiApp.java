d930c9b89c49e7df43e0954c2cf30f4edc401894
Maurits van der Schee <maurits@vdschee.nl>
2018-01-03 16:45:55 +0100

    some config

--- full/src/main/java/com/tqdev/crudapi/CrudApiApp.java
+++ full/src/main/java/com/tqdev/crudapi/CrudApiApp.java
@@ -1,21 +1,35 @@
 package com.tqdev.crudapi;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
 
 import com.tqdev.crudapi.memory.CrudApiServiceImpl;
 import com.tqdev.crudapi.service.CrudApiService;
 
 @SpringBootApplication(scanBasePackages = { "com.tqdev.crudapi" })
+@PropertySource("classpath:application.yml")
 public class CrudApiApp {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudApiApp.class, args);
 	}
 
+	@Value("${crud.driver.name}")
+	String crudDriverName;
+
 	@Bean
 	public CrudApiService crudApiService() {
-		return new CrudApiServiceImpl();
+		CrudApiService result;
+		switch (crudDriverName) {
+		case "memory":
+			result = new CrudApiServiceImpl();
+			break;
+		default:
+			result = null;
+		}
+		return result;
 	}
 }
