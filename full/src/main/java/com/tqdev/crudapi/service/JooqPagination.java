a27b399f927bb8d9bf28764680a5dad4a05a98b3
Maurits van der Schee <maurits@vdschee.nl>
2018-02-07 20:53:39 +0100

    Add pagination

---
+++ full/src/main/java/com/tqdev/crudapi/service/JooqPagination.java
@@ -0,0 +1,30 @@
+package com.tqdev.crudapi.service;
+
+public interface JooqPagination {
+
+	default public int offset(Params params) {
+		int offset = 0;
+		int numberOfRows = numberOfRows(params);
+		if (params.containsKey("page")) {
+			for (String key : params.get("page")) {
+				String[] parts = key.split(",", 2);
+				offset = (Integer.valueOf(parts[0]) - 1) * numberOfRows;
+			}
+		}
+		return offset;
+	}
+
+	default public int numberOfRows(Params params) {
+		int numberOfRows = 20;
+		if (params.containsKey("page")) {
+			for (String key : params.get("page")) {
+				String[] parts = key.split(",", 2);
+				if (parts.length > 1) {
+					numberOfRows = Integer.valueOf(parts[1]);
+				}
+			}
+		}
+		return numberOfRows;
+	}
+
+}
