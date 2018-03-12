af6ab00303b7ac4b483a5904e1952e223f741d70
Maurits van der Schee <maurits@vdschee.nl>
2018-03-12 22:35:29 +0100

    Try to add CORS headers

--- full/src/main/java/com/tqdev/crudapi/controller/CrudApiController.java
+++ full/src/main/java/com/tqdev/crudapi/controller/CrudApiController.java
@@ -8,7 +8,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tqdev.crudapi.core.CrudApiService;
