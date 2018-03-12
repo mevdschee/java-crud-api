f83e22de59cae5420e4c1ee250d0ca715425357a
Maurits van der Schee <maurits@vdschee.nl>
2018-03-12 23:59:46 +0100

    methods

--- full/src/main/java/com/tqdev/crudapi/config/CorsConfiguration.java
+++ full/src/main/java/com/tqdev/crudapi/config/CorsConfiguration.java
@@ -17,7 +17,7 @@ public class CorsConfiguration {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedMethods("OPTIONS, GET, PUT, POST, DELETE, PATCH")
+				registry.addMapping("/**").allowedMethods("OPTIONS", "GET", "PUT", "POST", "DELETE", "PATCH")
 						.allowedHeaders("Content-Type", "X-XSRF-TOKEN").allowedOrigins(allowedOrigins);
 			}
 		};
