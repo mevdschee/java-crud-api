1ffc1a0292f27a180b59f6b72407ae87d3263aef
Maurits van der Schee <maurits@vdschee.nl>
2018-03-30 20:44:36 +0200

    small improvement

--- full/src/main/java/com/tqdev/crudapi/controller/MetadataController.java
+++ full/src/main/java/com/tqdev/crudapi/controller/MetadataController.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +13,6 @@ import com.tqdev.crudapi.meta.CrudMetaService;
 
 @RestController
 @RequestMapping("/meta")
-@CrossOrigin(origins = "${rest.cors.allowed-origins:*}")
 public class MetadataController extends BaseController {
 
 	public static final Logger logger = LoggerFactory.getLogger(MetadataController.class);
